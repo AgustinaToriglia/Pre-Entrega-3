26844a96fcfa1628b868748a7c5a109127a179e7
AgustinaToriglia <agustina.toriglia@gmail.com>
2023-03-18 12:44:39 -0300

    final commit

--- main.js
+++ main.js
@@ -13,36 +13,6 @@ fetch(urlLocal)
     })
     .catch(error => console.log(error));
 
-// class Vehicle {
-//     constructor(id, brand, name, year, price, img) {
-//         this.id = id;
-//         this.brand = brand;
-//         this.name = name;
-//         this.year = year;
-//         this.price = price;
-//         this.img = img;
-//         this.amount = 1;
-//     }
-// }
-
-// const corsa = new Vehicle(1, "Chevrolet", "Corsa", "2017", 7000, "img/corsa.jpeg");
-// const kwid = new Vehicle(2, "Renault", "Kwid", "2018", 9000, "img/kwid.jpeg");
-// const suran = new Vehicle(3, "Volkswagen", "Suran", "2016", 8500, "img/suran.jpeg");
-// const p308 = new Vehicle(4, "Peugeot", "308", "2017", 10000, "img/308.jpeg");
-// const sandero = new Vehicle(5, "Renault", "Sandero", "2017", 12000, "img/sandero.jpeg");
-// const renegade = new Vehicle(6, "Jeep", "Renegade", "2018", 18500, "img/jeep.jpeg");
-// const spin = new Vehicle(7, "Chevrolet", "Spin", "2018", 21000, "img/spin.jpeg");
-// const ecosport = new Vehicle(8, "Ford", "Eco Sport", "2016", 14000, "img/Ecosport-2016.jpg");
-
-
-// const vehicles = [corsa, kwid, suran, p308, sandero, renegade, spin, ecosport];
-// console.log(vehicles);
-
-
-// if (localStorage.getItem("carrito")) {
-//     carrito = JSON.parse(localStorage.getItem("carrito"));
-// }
-
 const vehicleContainer = document.getElementById("vehicleContainer");
 
 const showVehicles = () => {
@@ -91,7 +61,7 @@ const addToCart = (id) => {
     } else {
         const vehicle = vehicles.find(vehicles => vehicles.id === id);
         const copy = JSON.parse(JSON.stringify(vehicle));
-        copy.amount=1
+        copy.amount = 1
         carrito.push(copy);
     }
     calcularTotal();
@@ -179,42 +149,34 @@ const calcularTotal = () => {
 
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 
-// vaciarCarrito.addEventListener("click", () => {
-//     eliminarTodoElCarrito();
-// })
 
 const eliminarTodoElCarrito = () => {
     localStorage.setItem("carrito", []);
     localStorage.setItem("mostrarCarrito", false);
     carrito = [];
-    
-    mostrarCarrito();
-    //localStorage.clear();
-    //console.log("localStorage", localStorage.getItem("carrito"));
-    //console.log("carrito", carrito);
-}
-vaciarCarrito.addEventListener("click", () => {
-    Swal.fire({
-        title: "¿Seguro que quieres vaciar el carrito?",
-        icon: "warning",
-        confirmButtonText: "Aceptar",
-        showCancelButton: true,
-        cancelButtonText: "Cancelar",
-        background: "lightslategray",
-    })
-    .then((result) => {
-        if (result.isConfirmed) {
-            console.log("voy a eliminar todo el carrito")
-            eliminarTodoElCarrito();
-            //console.log(carrito);
-            Swal.fire({
-                title: "Carrito Vacio",
-                icon: "success",
-                confirmButtonText: "Aceptar",
-    
-                background: "green",
-            })
-        }
-    })
-})
 
+    mostrarCarrito();
+    vaciarCarrito.addEventListener("click", () => {
+        Swal.fire({
+            title: "¿Seguro que quieres vaciar el carrito?",
+            icon: "warning",
+            confirmButtonText: "Aceptar",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            background: "lightslategray",
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    console.log("voy a eliminar todo el carrito")
+                    eliminarTodoElCarrito();
+                    Swal.fire({
+                        title: "Carrito Vacio",
+                        icon: "success",
+                        confirmButtonText: "Aceptar",
+
+                        background: "green",
+                    })
+                }
+            })
+    })
+}
